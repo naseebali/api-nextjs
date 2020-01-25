f843ed50961bafd1956f518dbb08d9ca602ac669
naseebali <naseebalina2@gmail.com>
2020-01-25 20:52:17 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,51 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from 'react';
+import fetch from 'isomorphic-unfetch';
+// import axios from 'axios';
+// import fetch from 'unfetch'
+// import useSWR from 'swr'
 
-const Home = () => (
+
+const Home = (props) => {
+  
+// const { data, error } = useSWR('api/users/', fetcher)
+
+//   if (error) return <div>failed to load</div>
+//   if (!data) return <div>loading...</div>
+//  {console.log(data)}
+//   return <div>Next stars: {data}</div>
+console.log(props);
+  return (
   <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
+    <input text="email" name="email" placeholder="email" />
   </div>
-)
+  )
 
-export default Home
+
+  // ou can Access to initial API setup using this url: https://lit-reef-54228.herokuapp.com/
+
+  // for signup: [POST] https://lit-reef-54228.herokuapp.com/api/auth/signup
+  // body parameters: name, email, password
+  
+  // for signin: [POST] https://lit-reef-54228.herokuapp.com/api/auth/signin
+  // body parameters: email, password
+  
+  // for logout: [POST] https://lit-reef-54228.herokuapp.com/api/auth/logout
+  // Headers: Authorization: Bearer {token}
+  
+  // for get current-user: [GET] https://lit-reef-54228.herokuapp.com/api/users/me
+  // Headers: Authorization: Bearer {token}
+
+
+  // return (
+  //   <div>
+
+  //   </div>
+  // )
+}
+Home.getInitialProps = async () => {
+  const res = await fetch('https://lit-reef-54228.herokuapp.com/api/users')
+  const data = await res.data;
+  // console.log(res);
+  return { stars: data }
+}
+export default Home;
